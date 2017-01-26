bfe5fd870855736f9455ea36c40614cdcc10d413
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-01-26 16:52:28 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+	min = 1000
+	result = nil
+	name_hash.each do |keys, values| 
+			if (values<min)
+				min = values
+				result = keys
+			end
+	end
+	result	
 end
